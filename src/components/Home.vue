1fba9b742c45db086dfa6ef4ca6c110f601cbf72
DDGG0531 <z887700101703027@gmail.com>
2018-05-17 11:57:19 +0800

    5-17-1157

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div> 
+    <!-- text main-->
+    <h1 class="test-text">main-color</h1> 
+    <h2 class="test-text-2">main-color-2</h2>
+     
+    <!--  -->
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import "~bootstrap/scss/bootstrap"
+@import "../assets/sass/all.sass"
+// @include media-breakpoint-up(xl)
+// 可以吃到sass variable
+.test-text-2
+  color: $main-color
+</style>
+
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+export default {
+  data() {
+    return {
+     
+    }
+  },
+  watch:{
+
+  },
+  methods: {
+    
+  },
+  mounted() {
+
+  },
+  created() {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+
+
+
---
+++ src/components/template.vue
@@ -0,0 +1,45 @@
+<template>
+  <div> 
+  </div>
+</template>
+
+
+<style lang="sass" scoped>
+@import "~bootstrap/scss/bootstrap"
+// @include media-breakpoint-up(xl)
+</style>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+export default {
+  data() {
+    return {
+     
+    }
+  },
+  watch:{
+
+  },
+  methods: {
+    
+  },
+  mounted() {
+
+  },
+  created() {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+
+
+
+
